fe26669f8bb12274318a48adcaf4cde5807ce57b
Ashraf M <ashrafcvd123@gmail.com>
2024-05-15 21:21:35 +0530

    header add

---
+++ script.js
@@ -0,0 +1,40 @@
+const imgs = document.querySelectorAll(".header-slider ul img")
+const controlRight = document.querySelector(".header-slider .right")
+const controlLeft = document.querySelector(".header-slider .left")
+
+let n = 0
+
+function changeSlider() {
+    for (let i = 0; i < imgs.length; i++) {
+            imgs[i].style.display = 'none'        
+    }
+    imgs[n].style.display = 'block'
+}
+
+changeSlider()
+
+controlLeft.addEventListener('click',() => {
+    
+    console.log('clicked');
+    if(n > 0){
+        n--
+    }else {
+        n = imgs.length-1
+    }
+    changeSlider();
+
+})
+
+controlRight.addEventListener('click',() => {
+
+    if(n < imgs.length-1){
+        n++
+    }else {
+        n = 0
+    }
+    changeSlider();
+
+})
+
+
+
